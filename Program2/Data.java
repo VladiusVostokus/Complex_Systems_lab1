8b73a2921bd321702fa6d89a3fafe9a3794eb3c3
VladiusVostokus <kovalenkov222@gmail.com>
2026-02-11 23:16:05 +0200

    add data class for program 2

---
+++ Program2/Data.java
@@ -0,0 +1,39 @@
+import java.util.Random;
+
+public class Data {
+    public int N, P, H;
+    public double a;
+    public double[] M, D, C;
+    public double[][] MC, MZ, MM;
+
+    public Data(int n) {
+        Random r = new Random();
+        N = n;
+        P = 4;
+        H = N / P;
+        a = generateDouble(r, 1, 10000);
+        M = new double[n];
+        D = new double[n];
+        C = new double[n];
+
+        MC = new double[n][n];
+        MZ = new double[n][n];
+        MM = new double[n][n];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                MC[i][j] = generateDouble(r, 1, 10);
+                MZ[i][j] = generateDouble(r, 1, 10);
+                MM[i][j] = generateDouble(r, 1, 10);
+            }
+            M[i] = generateDouble(r, 1, 10);
+            D[i] = generateDouble(r, 1, 10);
+            C[i] = generateDouble(r, 1, 10);
+        }
+    }
+
+    private double generateDouble(Random r, int rangeMin, int rangeMax) {
+        double randomDouble = rangeMin + (rangeMax - rangeMin) * r.nextDouble();
+        return randomDouble;
+    }
+}
